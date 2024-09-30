aff4668c38ee75c9afc742d9c449769de1954367
RobsonTrasel <robson.trasel@gmail.com>
2024-09-29 21:42:11 -0300

    fix: fix the argument, correct it's j - 1 not j - i

--- src/main/java/com/robsonf/Main.java
+++ src/main/java/com/robsonf/Main.java
@@ -9,13 +9,14 @@ public class Main {
 
         int m = nums1.length;
         int n = nums2.length;
-        int halfLen = (m + n) / 2;
+        int halfLen = (m + n + 1) / 2;
         int imin = 0, imax = m;
+
         while (imin <= imax) {
             int i = (imin + imax) / 2;
             int j = halfLen - i;
 
-            if (i < m && nums2[j - i] > nums1[i]) {
+            if (i < m && nums2[j - 1] > nums1[i]) {
                 imin = i + 1;
             } else if (i > 0 && nums1[i - 1] > nums2[j]) {
                 imax = i - 1;
@@ -44,13 +45,14 @@ public class Main {
 
                 return (maxLeft + minRight) / 2.0;
             }
-
         }
+
         throw new IllegalArgumentException("Arrays aren't sorted or valid");
     }
 
     public static void main(String[] args) {
         Main m = new Main();
+
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
         double result = m.findMedianSortedArrays(nums1, nums2);
